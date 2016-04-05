81c94226415f28f0baa0cc8f2b7d321acab28a90
Petros Splinakis <petros.splinakis@oracle.com>
2016-04-05 16:59:04 +0200

    Removing unnecessary configuration
    Signed-off-by: Petros Splinakis <petros.splinakis@oracle.com>

--- build/start-cluster.py
+++ build/start-cluster.py
@@ -7,7 +7,7 @@ nodemanager_path = domain_path + '/nodemanager'
 
 # Start Node Manager
 connect('weblogic',admin_pass)
-startNodeManager(NodeManagerHome=nodemanager_path, ListenPort='5556', ListenAddress='localhost', NativeVersionEnabled='false', block='true', username='weblogic', password='welcome1', domainName='domain')
+startNodeManager(NodeManagerHome=nodemanager_path, ListenPort='5556', ListenAddress='localhost', NativeVersionEnabled='false', block='true', username='weblogic', password=admin_pass, domainName=domain_name)
 
 # Start cluster
 start('DynamicCluster-0-Server-1', block='true')
